22e426145d0ffbe689c2365c819b9df177638076
tamnguyen1560@gmail.com <tamnguyen1560@gmail.com@8f0e06b2-a084-17bf-4d0a-d1d9cd0132ad>
2012-11-16 10:21:30 +0000

    commit project

---
+++ trunk/src/main/java/com/research/apache/poi/CellMappingBuilder.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.research.apache.poi;
+
+import java.util.List;
+
+
+/**
+ * @author tamnguyen
+ * 
+ */
+public interface CellMappingBuilder
+{
+
+	List<CellMapping> buildCellMappings();
+}
---
+++ trunk/src/main/java/com/research/apache/poi/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author tamnguyen
+ * 
+ */
+package com.research.apache.poi;
